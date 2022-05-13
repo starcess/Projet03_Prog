cbccdd28dba0c2aa5e06828bc2eb8b30233c4326
Rebecca Dauphnie Mombrun <joceline1233@gmail.com>
2022-05-13 12:18:14 -0400

    Class Livre

--- Projet03/src/classes/Livre.java
+++ Projet03/src/classes/Livre.java
@@ -9,12 +9,81 @@ package classes;
  * Author : Prénom, nom
  */
 public class Livre extends Volume{
+	private int annee;
+	private Genre genre;
+	private int nombreTotal;
+	private int nombreDisponible;
+	
+	
+	
+	
+	/**
+	 * @return the annee
+	 */
+	public int getAnnee() {
+		return annee;
+	}
 
 	/**
-	 * 
+	 * @param annee the annee to set
 	 */
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	/**
+	 * @return the genre
+	 */
+	public Genre getGenre() {
+		return genre;
+	}
+
+	/**
+	 * @param genre the genre to set
+	 */
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	/**
+	 * @return the nombreTotal
+	 */
+	public int getNombreTotal() {
+		return nombreTotal;
+	}
+
+	/**
+	 * @param nombreTotal the nombreTotal to set
+	 */
+	public void setNombreTotal(int nombreTotal) {
+		this.nombreTotal = nombreTotal;
+	}
+
+	/**
+	 * @return the nombreDisponible
+	 */
+	public int getNombreDisponible() {
+		return nombreDisponible;
+	}
+
+	/**
+	 * @param nombreDisponible the nombreDisponible to set
+	 */
+	public void setNombreDisponible(int nombreDisponible) {
+		this.nombreDisponible = nombreDisponible;
+	}
+
+	
 	public Livre() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public Livre(int annee1, Genre g, int nbTotal, int nbDispo) {
+		super();
+		setAnnee(annee1);
+		setGenre(g);
+		setNombreTotal(nbTotal);
+		setNombreDisponible(nbDispo);
 	}
 
 }
