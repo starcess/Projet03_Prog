24c3b90d19f2a7ba803743a79212afbab656e337
Rebecca Dauphnie Mombrun <joceline1233@gmail.com>
2022-05-19 16:17:29 -0400

    Added generation selon catégorie file and made enum class Categorie

---
+++ Projet03/src/classes/Categorie.java
@@ -0,0 +1,13 @@
+/* Fichier Categorie.java
+ * Auteur : rebec
+ * Date de création : May 19, 2022
+ */
+package classes;
+
+/**
+ *  TODO
+ * Author : Prénom, nom
+ */
+public enum Categorie {
+	LIVRE, BD, JOURNAL;
+}
