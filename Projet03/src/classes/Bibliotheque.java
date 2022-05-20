a085be71d2554801c5b787ea6db4b9bf06c268aa
Rebecca Dauphnie Mombrun <joceline1233@gmail.com>
2022-05-19 22:08:42 -0400

    Added stuff in class Bibliotheque, added Gson.jar, and did Serialisation class. ' I'm not completely sure it works though.

--- Projet03/src/classes/Bibliotheque.java
+++ Projet03/src/classes/Bibliotheque.java
@@ -4,18 +4,77 @@
  */
 package classes;
 
+import java.util.ArrayList;
+
 /**
  *  TODO
  * Author : Prénom, nom
  */
 public class Bibliotheque {
 
+	protected String nomBibliotheque = "Bibliothèque Ahuntsic";
+	protected final int MAX = 500;
+	protected ArrayList <Document> documents;
+	
+	
+	
+	@Override
+	public String toString() {
+		String msg ="";
+		return msg;
+	}
+	
+	
+	//Getters, setters and Constructors
+	/**
+	 * @return the nomBibliotheque
+	 */
+	public String getNomBibliotheque() {
+		return nomBibliotheque;
+	}
+
+
+	/**
+	 * @param nomBibliotheque the nomBibliotheque to set
+	 */
+	public void setNomBibliotheque(String nomBibliotheque) {
+		this.nomBibliotheque = nomBibliotheque;
+	}
+
+
+	/**
+	 * @return the documents
+	 */
+	public ArrayList<Document> getDocuments() {
+		return documents;
+	}
+
+
+	/**
+	 * @param documents the documents to set
+	 */
+	public void setDocuments(ArrayList<Document> documents) {
+		this.documents = documents;
+	}
+
+
+	/**
+	 * @return the mAX
+	 */
+	public int getMAX() {
+		return MAX;
+	}
+
+
 	/**
 	 * 
 	 */
 	public Bibliotheque() {
-		// TODO Auto-generated constructor stub
+		
+	}
 	
+	public Bibliotheque(String nom) {
+		setNomBibliotheque(nom);
 	}
 
 }
--- Projet03/src/util/Serialisation.java
+++ Projet03/src/util/Serialisation.java
@@ -4,17 +4,105 @@
  */
 package util;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+import classes.Document;
+
+
 /**
  *  TODO
  * Author : Prénom, nom
  */
 public class Serialisation {
-
+	
+									//Sérialisation json
+		
 	/**
 	 * 
+	 * @param documents
+	 * @param pathSauvegarde
+	 * @throws IOException
 	 */
-	public Serialisation() {
-		// TODO Auto-generated constructor stub
-	}
-
+	public static void objectToJson(ArrayList <Document> documents, String pathSauvegarde) throws IOException {
+		// Créer une instance Gson
+		Gson js = new Gson();
+		try (Writer out = new FileWriter(pathSauvegarde)) {
+		
+		js.toJson(documents, out);
+		}
+	}//fin méthode
+	
+	/**
+	* @param path
+	* @return
+	* @throws IOException
+	*/	 
+	
+	public static ArrayList <Document> jsonToObject(String path) throws IOException {
+		ArrayList<Document> documents;
+		
+		// Créer une instance Gson
+		Gson js = new Gson();
+		//JsonReader jsonReader ;
+		try (Reader in = new FileReader(path)) {
+			documents = js.fromJson(in, ArrayList.class);
+		}
+		return documents; 
+	}//fin méthode
+	
+	
+	
+									//Sérialisation Binaire
+	/**
+	 * 
+	 * @param documents
+	 * @param path
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public static void objetToBinaire(ArrayList <Document> documents, String path) throws FileNotFoundException, IOException {
+			try ( // Ouvre le fichier en ecriture
+				FileOutputStream fos = 
+				new FileOutputStream(path);
+				ObjectOutputStream serialiseur = 
+				new ObjectOutputStream(fos); )
+			{
+					// sérialiser l’objet
+					serialiseur.writeObject(documents);
+			}
+	}//fin méthode
+	
+	
+	
+	/**
+	 * 
+	 * @param path
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public static ArrayList <Document> binaireToObjet (String path) throws IOException, ClassNotFoundException{
+		try ( // Ouvre le fichier en lecture
+			FileInputStream fis = 
+			new FileInputStream(path);
+			ObjectInputStream deserialiseur = 
+			new ObjectInputStream(fis) )
+			{
+				// Désérialiser le flux en objet
+				ArrayList <Document> docs = (ArrayList <Document>) deserialiseur.readObject();
+				return docs;
+			}
+	}//fin deSerialise //fin methode	
 }
